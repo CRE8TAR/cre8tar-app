b8e3a9cd3de35bc3b0f6166ce1de4e49d00d86ee
C8 <cre8tar.tech@gmail.com>
2025-05-21 10:47:51 +0530

    Update TypewriterEffect.tsx

--- src/components/TypewriterEffect.tsx
+++ src/components/TypewriterEffect.tsx
@@ -29,7 +29,7 @@ const TypewriterEffect: React.FC<TypewriterEffectProps> = ({ text, delay = 50, c
   useEffect(() => {
     const cursorInterval = setInterval(() => {
       setShowCursor((current) => !current);
-    }, 500);
+    }, 150);
 
     return () => clearInterval(cursorInterval);
   }, []);
